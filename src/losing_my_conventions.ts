d573b51fc724515ffb9f4800fd86113fd62cbf82
nir@500tech.com <nir@500tech.com>
2016-12-04 18:09:10 +0200

    losing mty conventions

---
+++ src/losing_my_conventions.ts
@@ -0,0 +1,35 @@
+class LosingMyReligion {
+  
+  public artist: string;
+  public link  : string;
+
+  constructor() {
+    this.artist = "REM";
+    this.link   = "https://www.youtube.com/watch?v=xwtdhWltSIg";
+  }
+}
+
+class LosingMyConventions extends LosingMyReligion {
+
+  public partA: string = `
+    Oh class, it's bigger
+    It's bigger than ten
+    thousand lines
+    The lengths that you will go to
+    The distance in your lines
+    Oh no, you're using tabs
+    I stash it up...
+    `;
+
+  public partB: string = `
+    That's me in the office
+    That's me very late night
+    Losing my conventions
+    Trying to keep up with you
+    And I don't know if I can do it
+    Oh no, I've said too much
+    I haven't said enough
+    `;  
+}
+
+new ChangeDetector();
