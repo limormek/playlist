e111368cd8b0139acd45fdbca95735ca42784af9
Adam Klein <adam@500tech.com>
2016-12-04 23:52:24 +0200

    let it be

---
+++ src/let_it_be.ts
@@ -0,0 +1,49 @@
+class LetItBe {
+
+  public artist: string;
+  public link  : string;
+
+  constructor() {
+    this.artist = "The Beatles";
+    this.link   = "https://www.youtube.com/watch?v=rfX0q1aNCos";
+  }
+}
+
+class TBD extends LetItBe {
+
+  public partA: string = `
+    When I find myself in times of trouble,
+    No time for refactoring,
+    I use the words of wisdom - TBD.
+
+    And in my hour of darkness
+    When the codebase looks like spaghetti,
+    Write these words of wisdom - TBD.
+  `;
+
+  public chorusA: string = `
+    TBD, TBD, TBD, yeah TBD.
+    I will fix it later, TBD.
+  `;
+
+  public partA: string = `
+    And when the broken-syntax methods
+    Living in my code agree
+    There must be a stop to - TBD
+
+    ...
+
+  `;
+
+  public chorusB: string = `
+    TBD, TBD, TBD, yeah TBD.
+    I will fix it later, TBD.
+
+    TBD, TBD, TBD, yeah TBD.
+    No one will remember, TBD.
+  `;
+
+
+}
+
+new TBD();
